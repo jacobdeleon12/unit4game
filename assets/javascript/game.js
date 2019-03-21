c638db1ffdefe187c447fcf91d436d20475c7180
Jacob DeLeon <jacobdeleon12@gmail.com>
2019-03-21 09:40:00 -0700

    adding

---
+++ assets/javascript/game.js
@@ -0,0 +1,31 @@
+// varables
+
+var counter = 0;
+var wins = 0;
+var losses = 0;
+var img = ['assets/images/crystal1.png',
+    'assets/images/crystal2.png',
+    'assets/images/crystal3.png',
+    'assets/images/crystal4.png',
+];
+var randomResult = Math.floor(Math.random() * 101) + 19;
+
+
+// newGame function to start new game automaticly
+
+//displaying score to user
+    
+// creating 4 boxs with random values
+
+// getting number for crystals
+ 
+//adding on click event. 
+
+//using parseInt to read the values as numbers 
+
+//adding counter to value and pushing to dom 
+
+//checking if loss and calling the new game function
+
+//checking if win and calling the new game function
+
